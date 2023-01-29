53edddcabee22c5aa89eb7bd9fd2cf31223aad13
Caio Feliciano <caiorfeliciano@gmail.com>
2023-01-29 10:48:56 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+  const html = document.documentElement;
+
+  // if(html.classList.contains('light')){
+  //   html.classList.remove('light')
+  // } else {
+  //   html.classList.add('light')
+  // };
+
+  html.classList.toggle('light') // toggle remove e adiciona classe quando tem ou não tem no html
+
+//TROCAR A IMAGEM
+//   const img = document.querySelector('#profile img')
+
+//   if (html.classList.contains('light')) {
+//     img.setAttribute('src', './assets/sol.jpeg')
+//   } else {
+//     img.setAttribute('src', './assets/Avatar.png')
+//   };
+};
